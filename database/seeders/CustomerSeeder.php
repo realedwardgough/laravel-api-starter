98bb01bb0a224b0c28c54cc57674191df81b915c
Edward Gough <edwardggough@gmail.com>
2025-08-11 20:12:11 +0100

    chore(setup): setup customer and invoice models, including migrations and seeders

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Customer::factory()
+            ->count(25)
+            ->hasInvoices(10)
+            ->create();
+
+        Customer::factory()
+            ->count(50)
+            ->hasInvoices(3)
+            ->create();
+
+        Customer::factory()
+            ->count(26)
+            ->hasInvoices(0)
+            ->create();
+    }
+}
