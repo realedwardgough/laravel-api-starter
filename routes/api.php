a115ae2da21a92601959605a9b27823a5ff6fa78
Edward Gough <edwardggough@gmail.com>
2025-08-11 20:26:26 +0100

    feat(api): add test routing for seeded invoice data
    - Updated `api.php` to include routes for invoice testing
    - Modified `InvoiceController.php` to handle seeded data responses

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// API Version 1.0.0
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\v1'], function() {
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
+});
